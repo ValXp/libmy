3db8647b397ffd15af7109f967ec16d0d5ac7723
ValXp <valentin.horner@gmail.com>
2019-08-27 20:45:00 -0600

    -03 optim for libmy

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ mkdir -p obj
 pushd obj >> /dev/null
 rm -f *
 echo "Compiling " ../src/*.c
-cc -W -Werror -ansi -pedantic -I ../inc -c ../src/*.c
+cc -W -Werror -ansi -pedantic -O3 -I ../inc -c ../src/*.c
 ar r ../libmy.a *.o
 echo done
 popd >> /dev/null
